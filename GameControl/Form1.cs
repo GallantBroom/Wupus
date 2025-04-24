5992e7adfbfe6c828d13a38e6125850ceacf3556
GallantBroom <2010583@apps.nsd.org>
2025-04-24 12:56:40 -0700

    Added GameControl and GameControl Test.

---
+++ GameControl/Form1.cs
@@ -0,0 +1,31 @@
+namespace GameControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GameControl/GameControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameControl
+{
+    public class GameControl
+    {
+        public int HighScore { get; set; }
+
+
+        public GameControl() { 
+        
+                
+        
+         }
+        //start the game, go to the cave
+        public void startGame()
+        {
+           
+
+        }
+        // end the game, go back to the menu screen
+        public void endGame()
+        {
+           
+            
+
+
+        }
+        //call triva to do its thing
+        public void encounterWumpus()
+        {
+
+            
+
+
+
+        }
+        //interact with cave object and gamelocation to move the player to the correct room
+        public void playerMovesToNextRoom()
+        {
+
+        }
+        
+        
+
+
+
+      
+
+
+
+       
+
+
+
+    }
+}
