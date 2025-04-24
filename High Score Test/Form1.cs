a29c450baecb47feeba77fc63fc08677085c1fd4
User100-spec <1112414@apps.nsd.org>
2025-04-24 13:08:24 -0700

    Added HighScore

---
+++ High Score Test/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusTest
+{
+    
+    public partial class Form1 : Form
+    {
+        private Cave _Cave;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConstructorButton_Click(object sender, EventArgs e)
+        {
+            _Cave = new Cave();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ High Score Test/High Score Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_Score_Test
+{
+    internal class High_Score_Test
+    {
+
+        High_Score_Test high_Score = new High_Score_Test();
+
+
+    }
+}
