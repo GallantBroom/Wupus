d5ddc7996d5fe65e13b69f193efe3ba47c81fba5
User100-spec <1112414@apps.nsd.org>
2025-05-15 13:12:14 -0700

    Created an Add score and getscore method

--- High_Score/High_Score.cs
+++ High_Score/High_Score.cs
@@ -1,24 +1,71 @@
-﻿using System.Linq.Expressions;
+﻿using Microsoft.VisualBasic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.JavaScript;
 using System.Security.Cryptography.X509Certificates;
 
 namespace High_Score
 {
     public class High_Score
     {
+
+        /// <summary>
+        /// Making the constructor
+        /// </summary>
         public High_Score() { }
-        const string SCORES = "scores.json";
-        public string Number_of_turns { get; set; }
-        public string Number_of_gold_coins_left { get; set; }
-        public string Number_of_arrows_left { get; set; }
-        public string Wumpus_score { get; set; }
-        public High_Score(string n, string g, string a, string w)
+
+        private List<PlayerScore> scores = new List<PlayerScore>();
+        public List<PlayerScore> Scores 
         {
-            Number_of_turns = n;
-            Number_of_gold_coins_left = g;
-            Number_of_arrows_left = a;
-            Wumpus_score = w;
+            get
+            {
+                return scores.OrderByDescending(x => x.Score).ToList(); 
+            } 
         }
 
+        const string SCORES = "scores.json";
+       
+
+        public void AddScore(string name, int score, int caveSystem)
+        {
+            PlayerScore playerScore = new PlayerScore(name, score, caveSystem);
+            if (scores.Count < 10)
+            {
+                scores.Add(playerScore);
+            }
+            else
+            {
+                int lowestScore = score;
+                int lowestScoreIndex = -1;
+                for (int i = 0; i < scores.Count; i++)
+                {
+                    if (scores[i].Score < lowestScore)
+                    {
+                        lowestScore = scores[i].Score;
+                        lowestScoreIndex = i;
+                    }
+                }
+                if (lowestScoreIndex != -1)
+                {
+                    scores.RemoveAt(lowestScoreIndex);
+                    scores.Add(playerScore);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Calculating the players score
+        /// </summary>
+        private List<PlayerScore> _scores = new List<PlayerScore>();
+        public int CalculateScore(int n, int g, int a, int w)
+        {
+            int finalScore = 100 - n + g + (5 * a) + w;
+            return finalScore;
+        }
+        
     }
 }
  
---
+++ High_Score/PlayerScore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace High_Score
+{
+    public class PlayerScore
+    {
+        public PlayerScore(string name, int score, int caveSystem)
+        {
+            Name = name;
+            CaveSystem = caveSystem;
+            Score = score;
+        }
+
+        public int Score {  get; set; }
+        public string Name { get; set; }
+        public int CaveSystem { get; set; }  
+    }
+}
