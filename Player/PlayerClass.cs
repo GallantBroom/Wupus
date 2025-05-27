277518865a0b55738bfffa0549938cfe9f24ee88
Reed_B <61522578+meman111@users.noreply.github.com>
2025-05-27 13:25:28 -0700

    changed a little bit of player and game location

--- Player/PlayerClass.cs
+++ Player/PlayerClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,28 @@ namespace Player
 {
     public class PlayerClass
     {
-        public string name { get; set; }
-        public int score { get; set; }
-        public int position { get; set; }
-        public int arrows { get; set; }
-        public int gold { get; set; }
-        public int room { get; set; }
-        public bool alive { get; set; }
-        public PlayerClass(string name, int score, int position, int arrows, int gold, int room, bool alive)
+        public int Arrows { get; set; }
+        public int Coins { get; set; }
+        public int Turns { get; set; }
+        public int[] inv { get; set; }
+
+        public PlayerClass()
         {
-            this.name = name;
-            this.score = score;
-            this.position = position;
-            this.arrows = arrows;
-            this.gold = gold;
-            this.room = room;
-            this.alive = alive;
+            Arrows = 3; 
+            Coins = 5; 
+            Turns = 0;
+        }
+        public void arrowshot()
+        {
+            Arrows--;
         }
 
-        
+        public int PlayerMove()
+        {
+            Turns++;
+            Coins++;
+            return Turns;
+        }
+
     }
 }
